2782df59000fc0e240926db68abbe785f09904fa
Alberto <alberto@gmail.com>
2022-12-02 18:11:11 +0100

    act.main

--- app/src/main/java/com/learnby/navigation/Routes.kt
+++ app/src/main/java/com/learnby/navigation/Routes.kt
@@ -1,12 +1,8 @@
 package com.learnby.navigation
 
 sealed class Routes (val route: String){
-<<<<<<< HEAD
-    object  Login : Routes("Login")
-    object Questions : Routes("Questions")
-=======
     object Login : Routes("Login")
     object Questions : Routes("Questions")
 
->>>>>>> Alberto
+
 }
