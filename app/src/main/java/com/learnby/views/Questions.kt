aee79aa8bcf2b8a9a9c84a3d14ad587dea6d2229
Justin Rondon <rondonjustin643@hotmail.com>
2022-12-02 19:36:54 +0100

    Creo la base de la vista Questions
    creo la vista questions con lo básico para trabajar

--- app/src/main/java/com/learnby/views/Questions.kt
+++ app/src/main/java/com/learnby/views/Questions.kt
@@ -1,25 +1,22 @@
 package com.learnby.views
 
-import android.graphics.Paint.Style
-import android.os.Bundle
-import android.view.Surface
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.sp
-import androidx.navigation.NavHostController
 import com.learnby.ui.theme.LearnByTheme
 
 @Composable
@@ -31,40 +28,93 @@ fun VistaQuestion() {
             .fillMaxHeight()
             .background(Color(0xFFFFFF)),
             horizontalAlignment  = Alignment.CenterHorizontally
+
     ) {
+
         Text(
             text = "Python Quiz",
-            fontSize = 30.sp
+            fontSize = 30.sp,
         )
         Row (
             modifier = Modifier
                 .padding(35.dp)
                 ){
-            Text(
-                text = "Aciertos"
+            Button(onClick = { /*TODO*/ }, shape =RoundedCornerShape(100.dp)) {
+                Text(
+                    text = "5 ",
+                    fontSize = 20.sp
+                )
+                Text(
+                    text = "Aciertos",
+                    fontSize = 20.sp
+                )
+            }
+
+            Spacer(modifier = Modifier.width(20.dp))
+
+            Button(onClick = { /*TODO*/ }, shape = RoundedCornerShape(100.dp)){
+                Text(
+                    text = "3 ",
+                    fontSize = 20.sp
+                )
+                Text(
+                        text = "Fallos",
+                fontSize = 20.sp
+                )
+            }
+        }
+
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.background(Color.Gray, shape = RoundedCornerShape(100.dp))
+                .padding(bottom = 50.dp)
+        ) {
+            val image = painterResource(com.example.learnby.R.drawable.logopython)
+            Image(
+                painter = image,
+                contentDescription = null,
+                modifier = Modifier
+                    .size(260.dp)
+                    .padding(10.dp),
+
             )
+
             Text(
-                text = "Fallos"
+                text = "Aqui aparecera la pregunta",
+                color = Color.White,
+                textAlign = TextAlign.Center,
+                fontSize = 36.sp,
             )
         }
-        val image = painterResource(id = R.drawable._848152fcef1014c0b5e4967)
-        Image(painter = image, contentDescription = null)
-        /*Button(onClick = { /*TODO*/ }) {
-            Text("hola")
+        
+        Column(modifier = Modifier.padding(5.dp)) {
+            Row() {
+                Button(onClick = { /*TODO*/ },shape = RoundedCornerShape(14.dp)) {
+                    Text(text = "Opcion A", fontSize = 20.sp)
+                }
+                Spacer(modifier = Modifier.width(40.dp))
+                Button(onClick = { /*TODO*/ },shape = RoundedCornerShape(14.dp)) {
+                    Text(text = "Opcion B",fontSize = 20.sp)
+                }
+            }
+            Row(
+                modifier = Modifier.padding( top = 54.dp),
+                horizontalArrangement = Arrangement.Center
+            ) {
+                Button(onClick = { /*TODO*/ },shape = RoundedCornerShape(14.dp)) {
+                    Text(text = "Opcion C",fontSize = 20.sp)
+                }
+                Spacer(modifier = Modifier.width(40.dp))
+                Button(onClick = { /*TODO*/ },shape = RoundedCornerShape(14.dp  )) {
+                    Text(text = "Opcion D",fontSize = 20.sp)
+                }
+            }
+
         }
-        Text(
-            text = "hola",
-            fontWeight = FontWeight.Bold,
-            modifier = Modifier.padding(top = 24.dp, bottom = 8.dp)
-        )
-        Text(
-            text = "hola",
-            fontSize = 16.sp
-        )*/
     }
 }
 
-@Preview(showBackground = true)
+@Preview()
 @Composable
 fun TaskCompletedPreview() {
     LearnByTheme() {
