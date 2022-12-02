eb6dee4f69e9346a125734729088d8274274a5dd
Alberto <alberto@gmail.com>
2022-12-02 17:54:39 +0100

    routes update 2.0

--- app/src/main/java/com/learnby/navigation/Routes.kt
+++ app/src/main/java/com/learnby/navigation/Routes.kt
@@ -1,5 +1,6 @@
 package com.learnby.navigation
 
 sealed class Routes (val route: String){
-   object Login : Routes("Login")
+    object  Login : Routes("Login")
+    object Questions : Routes("Questions")
 }
