92d6d6ab17c08fa0b9b644e6910e63aca835d5ba
Alberto <albertogarvara@gmail.com>
2023-05-29 12:42:00 +0200

    pruebas de vista perfil

---
+++ app/src/main/java/com/learnby/views/Profile.kt
@@ -0,0 +1,21 @@
+package com.learnby.navigation
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.learnby.ui.theme.LearnByTheme
+
+@Composable
+fun Profile() {
+    Column() {
+        
+    }
+}
+
+@Preview
+@Composable
+fun PreviewProfile() {
+    LearnByTheme {
+        Profile()
+    }
+}
