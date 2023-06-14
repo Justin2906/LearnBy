7a97b98a0df5a036d2a422449fd33bc83c4845f1
Alberto <albertogarvara@gmail.com>
2023-06-14 22:14:27 +0200

    cambiar foto de perfil

--- app/src/main/java/com/learnby/views/editProfile.kt
+++ app/src/main/java/com/learnby/views/editProfile.kt
@@ -1,13 +1,19 @@
 package com.learnby.views
 
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
@@ -20,9 +26,12 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import androidx.core.net.toUri
 import androidx.navigation.NavController
+import coil.compose.rememberImagePainter
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.UserProfileChangeRequest
 
@@ -52,7 +61,10 @@ fun EditProfile(navController: NavController) {
     val user = auth.currentUser
     val newEmailState = rememberSaveable { mutableStateOf(user?.email ?: "") }
     val newUserNameState = rememberSaveable { mutableStateOf(user?.displayName ?: "") }
-
+    val newPhotoState = rememberSaveable { mutableStateOf(user?.photoUrl ?: "") }
+    val launcher = rememberLauncherForActivityResult(ActivityResultContracts.GetContent()) { uri ->
+        newPhotoState.value = uri.toString()
+    }
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -60,6 +72,21 @@ fun EditProfile(navController: NavController) {
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+
+        Row(horizontalArrangement = Arrangement.Center) {
+            if (user != null) {
+                if (user.photoUrl?.isAbsolute == true) {
+                    Image(
+                        painter = rememberImagePainter(user.photoUrl),
+                        contentDescription = "Avatar",
+                        modifier = Modifier
+                            .size(80.dp)
+                            .clip(CircleShape)
+                            .border(2.dp, Color.Gray, CircleShape)
+                    )
+                }
+            }
+        }
         Text(
             text = "Editar perfil",
             style = MaterialTheme.typography.h4,
@@ -83,20 +110,29 @@ fun EditProfile(navController: NavController) {
             )},
 
         )
+        
         Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceEvenly) {
-            Button(onClick = { saveChanges(newEmailState.value, newUserNameState.value, auth) }) {
+            Button(onClick = { launcher.launch("image/*")}) {
+                Text(text = "Cambiar foto de perfil",
+                    color = Color.Black
+                )
+            }
+            Button(onClick = { saveChanges(newEmailState.value, newUserNameState.value,
+                newPhotoState.value as String, auth) }) {
                 Text(
                     text = "Guardar cambios",
                     color = Color.Black
                 )
             }
+            
         }
     }
 }
 
-private fun saveChanges(newEmail: String, newUserName: String, auth: FirebaseAuth) {
+private fun saveChanges(newEmail: String, newUserName: String, newPhotoUrl: String, auth: FirebaseAuth) {
     val user = auth.currentUser
     val profileUpdates = UserProfileChangeRequest.Builder()
+        .setPhotoUri(newPhotoUrl.toUri())
         .setDisplayName(newUserName)
         .build()
 
